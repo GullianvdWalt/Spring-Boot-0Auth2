4854c528f336ccca8bdcd4d2a41ff7d6449f6d7b
Gullian <gullianvdwalt1@gmail.com>
2021-04-17 08:04:17 +0200

    init

---
+++ src/main/java/com/gvdw/springbootoauth/SpringBootOauthApplication.java
@@ -0,0 +1,13 @@
+package com.gvdw.springbootoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootOauthApplication.class, args);
+    }
+
+}
